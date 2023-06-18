a3e70e228672155bace688932a8c672e3c99f5de
Brendan Collins <bcollin4@umd.edu>
2023-06-18 14:53:08 -0400

    Push

--- vertMovementScript.js
+++ vertMovementScript.js
@@ -9,11 +9,15 @@ document.addEventListener('keyup', function (e) {
 AFRAME.registerComponent('vert-movement-listener', {
     tick: function () {
         if(map['ControlLeft']){
-            console.log("Lctrl")
             this.el.object3D.position.y -= 0.1
         }else if(map['Space']){
-            console.log("space")
             this.el.object3D.position.y += 0.1
         }
     }
+});
+
+document.addEventListener("mousemove", function (e) {
+    
+    menuCam.components['look-controls'].pitchObject.rotation.set(Math.PI/180*(menuCam.components['look-controls'].pitchObject.rotation.x + e.movementX),0,0);
+    menuCam.components['look-controls'].yawObject.rotation.set(0,Math.PI/180*(menuCam.components['look-controls'].pitchObject.rotation.y + e.movementY),0);
 });
